f12582caf368d80dba7383bc283a1a5d5b960dac
Bas Buller <bas.buller@gmail.com>
2023-02-15 20:37:41 +0000

    automatically box types at creation

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ trait Lazy {
     fn execute(&self) -> Value;
 }
 trait UnaryOp<T: Lazy> {
-    fn create(value: Box<T>) -> Self;
+    fn new(value: Box<T>) -> Box<Self>;
 }
 trait BinaryOp<T: Lazy, U: Lazy> {
-    fn create(left_value: Box<T>, right_value: Box<U>) -> Self;
+    fn new(left_value: Box<T>, right_value: Box<U>) -> Box<Self>;
 }
 
 
@@ -22,8 +22,8 @@ struct Negate<T: Lazy> {
     value: Box<T>,
 }
 impl<'a, T: Lazy> UnaryOp<T> for Negate<T> {
-    fn create(value: Box<T>) -> Negate<T> {
-        Negate{value: value}
+    fn new(value: Box<T>) -> Box<Negate<T>> {
+        Box::new(Negate{value: value})
     }
 }
 impl<T: Lazy> Lazy for Negate<T> {
@@ -39,8 +39,8 @@ struct Add<T: Lazy, U: Lazy> {
     right_value: Box<U>,
 }
 impl<'a, T: Lazy, U: Lazy> BinaryOp<T, U> for Add<T, U> {
-    fn create(left_value: Box<T>, right_value: Box<U>) -> Add<T, U> {
-        Add{left_value: left_value, right_value: right_value}
+    fn new(left_value: Box<T>, right_value: Box<U>) -> Box<Add<T, U>> {
+        Box::new(Add{left_value: left_value, right_value: right_value})
     }
 }
 impl<'a, T: Lazy, U: Lazy> Lazy for Add<T, U> {
@@ -49,15 +49,15 @@ impl<'a, T: Lazy, U: Lazy> Lazy for Add<T, U> {
     }
 }
 
-fn subtract<T: Lazy, U: Lazy>(left_value: Box<T>, right_value: Box<U>) -> Add<T, Negate<U>> {
-    let neg_right_value = Box::new(Negate::create(right_value));
-    Add::create(left_value, neg_right_value)
+fn subtract<T: Lazy, U: Lazy>(left_value: Box<T>, right_value: Box<U>) -> Box<Add<T, Negate<U>>> {
+    let neg_right_value = Negate::new(right_value);
+    Add::new(left_value, neg_right_value)
 }
 
 
 fn main() {
-    let ast0 = Box::new(Negate::create(Box::new(1.0)));
-    let ast1 = Box::new(Add::create(ast0, Box::new(5.0)));
+    let ast0 = Negate::new(Box::new(1.0));
+    let ast1 = Add::new(ast0, Box::new(5.0));
     let ast = subtract(ast1, Box::new(10.0));
     println!("{:?}", ast);
     println!("{:?}", ast.execute());
