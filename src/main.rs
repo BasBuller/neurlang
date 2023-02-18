343bb91031be8632cc6907ae159caa75501e017e
Bas Buller <bas.buller@gmail.com>
2023-02-18 22:10:33 +0000

    Made the AST generic over the data type by capturing the elementary execution functions in a trait, which in turn is part of the trait bound for the AST methods.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,61 @@
-type Value = f64;
-
 #[derive(Debug)]
-enum AST<'a> {
+enum AST<'a, T: ExecuteAST> {
     Value {
-        value: Value,
+        value: T,
     },
     Negate {
-        value: &'a AST<'a>,
+        value: &'a AST<'a, T>,
     },
     Add {
-        left_value: &'a AST<'a>,
-        right_value: &'a AST<'a>,
+        left_value: &'a AST<'a, T>,
+        right_value: &'a AST<'a, T>,
     },
 }
 
-impl<'a> AST<'a> {
-    fn new(value: Value) -> Box<AST<'a>> {
+impl<'a, T: ExecuteAST> AST<'a, T> {
+    fn new(value: T) -> Box<AST<'a, T>> {
         Box::new(AST::Value { value })
     }
-    
-    fn negate(&'a self) -> Box<AST<'a>> {
+
+    fn negate(&'a self) -> Box<AST<'a, T>> {
         Box::new(AST::Negate { value: self })
     }
-    
-    fn add(&'a self, value: &'a AST) -> Box<AST<'a>> {
-        Box::new(AST::Add { left_value: self, right_value: value })
+
+    fn add(&'a self, value: &'a AST<'a, T>) -> Box<AST<'a, T>> {
+        Box::new(AST::Add {
+            left_value: self,
+            right_value: value,
+        })
     }
-    
-    fn execute_cpu(&self) -> Value {
+
+    fn execute(&self) -> T {
         match self {
-            AST::Value { value } => *value,
-            AST::Negate { value } => -value.execute_cpu(),
-            AST::Add { left_value, right_value } => left_value.execute_cpu() + right_value.execute_cpu(),
+            AST::Value { value } => value.value(),
+            AST::Negate { value } => value.execute().negate(),
+            AST::Add {
+                left_value,
+                right_value,
+            } => left_value.execute().add(right_value.execute()),
         }
+    }
+}
+
+trait ExecuteAST {
+    fn value(&self) -> Self;
+    fn negate(&self) -> Self;
+    fn add(&self, right_value: Self) -> Self;
+}
+
+type Value = f64;
+impl ExecuteAST for Value {
+    fn value(&self) -> Self {
+        *self
+    }
+    fn negate(&self) -> Self {
+        -*self
+    }
+    fn add(&self, right_value: Self) -> Self {
+        *self + right_value
     }
 }
 
@@ -43,5 +66,5 @@ fn main() {
     let ast = ast.add(temp);
 
     println!("{:?}", ast);
-    println!("{:?}", ast.execute_cpu());
+    println!("{:?}", ast.execute());
 }
