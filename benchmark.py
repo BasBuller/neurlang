a0fe3a4c4ab5387c733a6a94dc88bb10b066994c
Bas Buller <bas.buller@gmail.com>
2023-03-16 22:24:30 +0000

    added pytorch 2.0 to the benchmark, for how simple the benchmark is pytorch is king

--- benchmark.py
+++ benchmark.py
@@ -2,7 +2,7 @@ import timeit
 
 import numpy as np
 import jax
-
+import torch
 
 
 def numpy_function():
@@ -10,6 +10,24 @@ def numpy_function():
     def time_func():
         res = values * values + values * 2
         return res
+    return time_func
+
+
+def torch_function_slow():
+    values = torch.randn(5000, 5000)
+    def time_func():
+        res = values * values + values * 2
+        return res
+    return time_func
+
+
+def torch_function_fast():
+    values = torch.randn(5000, 5000)
+    @torch.compile
+    def time_func():
+        res = values * values + values * 2
+        return res
+    time_func()
     return time_func
 
 
@@ -32,6 +50,11 @@ def jax_function_fast():
 
 
 if __name__ == "__main__":
+    jax_function_fast()()
+    torch_function_fast()()
+
     print("Numpy: ", timeit.timeit(numpy_function(), number=10) / 10)
+    print("Torch slow: ", timeit.timeit(torch_function_slow(), number=10) / 10)
+    print("Torch fast: ", timeit.timeit(torch_function_fast(), number=10) / 10)
     print("Jax slow: ", timeit.timeit(jax_function_slow(), number=10) / 10)
     print("Jax fast: ", timeit.timeit(jax_function_fast(), number=10) / 10)
